bac897bd3fc16ba29c695ca00d83e9004619f590
Harsha <harshavardhanambatias@gmail.com>
2026-03-25 14:45:40 +0530

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 FROM python:3.10-slim
 
 WORKDIR /app
@@ -8,19 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 5000
+EXPOSE 8080
 
-=======
-FROM python:3.10-slim
-
-WORKDIR /app
-
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . .
-
-EXPOSE 5000
-
->>>>>>> 74eaa8f439ed57fa0347f08dbf9cda7d41e014d2
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,15 +7,4 @@ def home():
     return "Hello from End-to-End DevOps Project"
 
 if __name__ == '__main__':
-=======
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello from End-to-End DevOps Project"
-
-if __name__ == '__main__':
->>>>>>> 74eaa8f439ed57fa0347f08dbf9cda7d41e014d2
     app.run(host='0.0.0.0', port=8080)
